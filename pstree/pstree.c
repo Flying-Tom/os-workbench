9f599be32b850be3e02f11ddca6f5c352e7b65ef
tracer-nju <tracer@nju.edu.cn>
2021-03-08 21:31:47 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 1cfb4895b62049dc95563d8035ff507e   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-41-generic       Architecture: x86-64  21:31:47 up  4:29,  1 user,  load average: 0.00, 0.02, 0.04

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include <string.h>
 
-bool show_pids = false, numeric_sort = false, version = false;
+int show_pids = 0, numeric_sort = 0, version = 0;
 
 void ParameterMatch(int argc, char *argv[])
 {
@@ -11,11 +11,11 @@ void ParameterMatch(int argc, char *argv[])
     {
         assert(argv[i]);
         if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0)
-            show_pids = true;
+            show_pids = 1;
         if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--numeric-sort") == 0)
-            numeric_sort = true;
+            numeric_sort = 1;
         if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0)
-            version = true;
+            version = 1;
         printf("argv[%d] = %s\n", i, argv[i]);
     }
     assert(!argv[argc]);
