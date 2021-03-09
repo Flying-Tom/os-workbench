241dc6ead390aadaa511d4847e5c20818a0495fe
tracer-nju <tracer@nju.edu.cn>
2021-03-09 13:28:19 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7e46a0392ba743d3b288a60dcd502beb   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-41-generic       Architecture: x86-64  13:28:19 up  1:04,  0 users,  load average: 0.05, 0.07, 0.08

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -89,7 +89,7 @@ int main(int argc, char *argv[])
                 char stat_buf[512];
                 sprintf(stat_buf, "/proc/%s/stat", dir->d_name);
                 FILE *fp = fopen(stat_buf, "r");
-                fscanf(fp, "%*s (%s) %*s %*s %d", process[process_cnt].name, &process[process_cnt].ppid);
+                fscanf(fp, "%*s (%s) %*s %d", process[process_cnt].name, &process[process_cnt].ppid);
                 fclose(fp);
                 process[process_cnt].name[strlen(process[process_cnt].name) - 1] = '\0';
                 process_cnt++;
