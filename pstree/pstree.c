caad3f1621373d2dcb6fa9c2dcfc5fb61a14cdbb
tracer-nju <tracer@nju.edu.cn>
2021-03-09 21:22:41 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7e46a0392ba743d3b288a60dcd502beb   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-41-generic       Architecture: x86-64  21:22:41 up  8:58,  0 users,  load average: 0.00, 0.03, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -87,7 +87,7 @@ int line_rec[16] = {};
 
 void PrintProcessTree(struct Process *cur, int deepth)
 {
-    for (int i = 0; i < deepth; i++)
+    for (int i = 1; i < deepth; i++)
     {
         if (line_rec[i])
             printf("|");
@@ -103,7 +103,7 @@ void PrintProcessTree(struct Process *cur, int deepth)
     else
         printf("%s\n", cur->name + 1);
 
-    //line_rec[deepth] = 1;
+    line_rec[deepth] = 1;
     for (int i = 0; i < cur->children_num; i++)
     {
         for (int i = 0; i <= deepth + 1; i++)
