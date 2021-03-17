c0927e68f2d0ef9531efa5fe32c17b13e357a5f8
tracer-nju <tracer@nju.edu.cn>
2021-03-17 20:42:25 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 81f68f319b2646a7a0762b681bd894d6   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  20:42:26 up  7:07,  1 user,  load average: 0.07, 0.03, 0.00

--- libco/co.c
+++ libco/co.c
@@ -1,15 +1,45 @@
 #include "co.h"
 #include <stdlib.h>
+#include <setjmp.h>
 
-struct co {
+#define STACK_SIZE 64 * 1024
+
+enum co_status
+{
+    CO_NEW = 1,
+    CO_RUNNING,
+    CO_WAITING,
+    CO_DEAD,
 };
 
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+struct co
+{
+    char *name;
+    void (*func)(void *);
+    void *arg;
+
+    enum co_status status;
+    struct co *waiter;
+    jmp_buf context;
+    uint8_t stack[STACK_SIZE];
+};
+
+struct co *co_start(const char *name, void (*func)(void *), void *arg)
+{
+    struct co *new_co = malloc(sizeof(co));
+    new_co->name = name;
+    new_co->func = func;
+    new_co->arg = arg;
+
+    return new_co;
 }
 
-void co_wait(struct co *co) {
+void co_wait(struct co *co)
+{
+    (*co)();
+    free(co);
 }
 
-void co_yield() {
+void co_yield()
+{
 }
