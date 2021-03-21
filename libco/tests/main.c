e714d3dafe74406c44821bc7b79883de97a77747
tracer-nju <tracer@nju.edu.cn>
2021-03-21 18:25:51 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7e89f5ab687f47df9c5359dfb6718f61   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  18:25:51 up  6:25,  0 users,  load average: 0.18, 0.06, 0.01

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -34,7 +34,7 @@ static void work(void *arg)
 
 static void test_1()
 {
-
+    puts("test1 start?");
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
 
