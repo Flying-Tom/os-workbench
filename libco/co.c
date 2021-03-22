9c3d7faf1bb132fce39ec52839e436ded56ce2dd
tracer-nju <tracer@nju.edu.cn>
2021-03-22 18:06:30 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7e89f5ab687f47df9c5359dfb6718f61   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  18:06:30 up 1 day,  6:05,  0 users,  load average: 0.16, 0.11, 0.06

--- libco/co.c
+++ libco/co.c
@@ -147,7 +147,7 @@ void co_yield()
                 next_co = next_co->prev;
             }
             //printf("next_co->status:%d\n", next_co->status);
-            printf("co_group_cnt:%d", co_group_cnt);
+            printf("co_group_cnt:%d\n", co_group_cnt);
         } while (next_co->status == CO_UNDEFINE || next_co->status == CO_DEAD);
         //printf("switch to: %s %d\n", next_co->name, next_co->status);
         coroutine_switch(next_co);
