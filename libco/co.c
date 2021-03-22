b3ac32fb5d66dcc3f058f6af1bddd6f2ffca4cc3
tracer-nju <tracer@nju.edu.cn>
2021-03-22 17:11:49 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7e89f5ab687f47df9c5359dfb6718f61   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  17:11:49 up 1 day,  5:11,  0 users,  load average: 0.07, 0.03, 0.00

--- libco/co.c
+++ libco/co.c
@@ -67,7 +67,7 @@ void coroutine_switch(struct co *co)
     switch (co->status)
     {
     case CO_NEW:
-        stack_switch_call(co->stack, coroutine_entry, (uintptr_t)co);
+        stack_switch_call(&co->stack[STACK_SIZE], coroutine_entry, (uintptr_t)co);
         break;
     case CO_RUNNING:
         longjmp(co_current->context, 1);
