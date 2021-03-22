f94836ae9945ba4c3fb2fc6fdbfd4413b9e4a626
tracer-nju <tracer@nju.edu.cn>
2021-03-22 17:42:09 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7e89f5ab687f47df9c5359dfb6718f61   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  17:42:09 up 1 day,  5:41,  0 users,  load average: 0.07, 0.04, 0.00

--- libco/co.c
+++ libco/co.c
@@ -58,7 +58,9 @@ void coroutine_entry(struct co *co)
     co->status = CO_RUNNING;
     co->func(co->arg);
     co->status = CO_DEAD;
-    puts("coroutine_entry finished");
+    if (co->waiter)
+        co->waiter->status = CO_RUNNING;
+    co_yield();
 }
 
 void coroutine_switch(struct co *co)
