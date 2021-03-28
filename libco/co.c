2be90bb39c6268e6c8f1b161f646a83ba3b135cf
tracer-nju <tracer@nju.edu.cn>
2021-03-28 19:31:37 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5c0a327510c64a908771c3b22ae425d7   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  19:31:37 up 12 min,  0 users,  load average: 0.00, 0.04, 0.06

--- libco/co.c
+++ libco/co.c
@@ -122,6 +122,7 @@ void co_yield()
         case CO_NEW:
             puts("co_new");
             stack_switch_call((void *)((((uintptr_t)co_current->stack >> 4) << 4) + STACK_SIZE), coroutine_entry, (uintptr_t)co_current);
+            //stack_switch_call((void *)((((uintptr_t)co_current->stack >> 4) << 4) + STACK_SIZE), coroutine_entry, (uintptr_t)co_current);
             //puts("out");
             break;
         case CO_RUNNING:
