5cdf7ceef41f43d2b27b2c2a8539a34aa9c75e0e
tracer-nju <tracer@nju.edu.cn>
2021-03-28 21:45:46 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5c0a327510c64a908771c3b22ae425d7   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  21:45:46 up  2:26,  0 users,  load average: 0.36, 0.13, 0.04

--- libco/co.c
+++ libco/co.c
@@ -7,7 +7,7 @@
 #include <assert.h>
 
 #define STACK_SIZE 64 * 1024
-#define CO_MAXNUM 256
+#define CO_MAXNUM 128
 
 enum co_status
 {
@@ -26,13 +26,14 @@ struct co
 
     enum co_status status;
     struct co *waiter;
+    struct co *next;
 
     jmp_buf context;
     uint8_t stack[STACK_SIZE];
-}; // root coroutine
+} co_main; // root coroutine
 
-struct co *co_current;
-struct co co_group[CO_MAXNUM];
+struct co *co_list_head = &co_main;
+struct co *co_current = &co_main;
 
 int co_group_cnt;
 
@@ -52,87 +53,86 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
     );
 }
 
-void co_del(struct co *co)
-{
-    if (co_group_cnt == 0)
-        return;
-    if (co != &co_group[co_group_cnt - 1])
-    {
-        //assert(co_group[co_group_cnt - 1].status != CO_DEAD);
-        while (co_group[co_group_cnt - 1].status == CO_DEAD)
-        {
-            co_group_cnt--;
-            puts("fucks");
-        }
-
-        memmove(co, &co_group[co_group_cnt - 1], sizeof(struct co));
-        co_group_cnt--;
-    }
-    else
-    {
-        co_group_cnt--;
-    }
-}
-
 void coroutine_entry(struct co *co)
 {
-    //puts("coroutine_entry");
-    //printf("%s\n", co->name);
     co->status = CO_RUNNING;
     co->func(co->arg);
     co->status = CO_DEAD;
+    //co_group_cnt--; can't be there because the list need co_group_cnt to determine the specific element, and here co isn't released yet
     if (co->waiter)
         co->waiter->status = CO_RUNNING;
-    co_del(co);
     co_yield();
 }
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg)
 {
-    co_group[co_group_cnt].name = (char *)name;
-    co_group[co_group_cnt].func = func;
-    co_group[co_group_cnt].arg = arg;
-    co_group[co_group_cnt].status = CO_NEW;
-    //puts("co_start finished");
-    return &co_group[co_group_cnt++];
+
+    struct co *new_co = malloc(sizeof(struct co));
+    new_co->name = (char *)name;
+    new_co->func = func;
+    new_co->arg = arg;
+    new_co->status = CO_NEW;
+
+    co_list_head->next = new_co;
+    co_list_head = co_list_head->next;
+
+    co_group_cnt++;
+    return new_co;
 }
 
 void co_wait(struct co *co)
 {
     //printf("co_wait(%s) status:%d\n", co->name, co->status);
-    co_current->status = CO_WAITING;
-    co->waiter = co_current;
-    while (co->status != CO_DEAD)
-        co_yield();
-    //printf("co_current->status:%d\n", co_current->status);
-    co_current->status = CO_RUNNING;
+    if (co->status != CO_DEAD)
+    {
+        co_current->status = CO_WAITING;
+        co->waiter = co_current;
+        while (co->status != CO_DEAD)
+            co_yield();
+        //printf("co_current->status:%d\n", co_current->status);
+        co_current->status = CO_RUNNING;
+    }
+    else
+    {
+        struct co *co_temp = &co_main;
 
-    co_del(co);
-    //puts("free end");
+        while (co_temp->next != co)
+            co_temp = co_temp->next;
+        co_temp->next = co->next;
+        co_group_cnt--;
+        free(co);
+    }
 }
 
 void co_yield()
 {
     //puts("co_yield");
+    //printf("%s status:%d\n", co_main.name, co_main.status);
     int val = setjmp(co_current->context);
+    //printf("%s %d  val:%d\n", co_current->name, co_current->status, val);
     if (val == 0)
     {
-        struct co *next_co = NULL;
+        int next_co_id;
+        struct co *next_co;
         do
         {
-            int next_co_id = rand() % co_group_cnt;
-            next_co = &co_group[next_co_id];
-            printf("%d\n", next_co->status);
+            next_co_id = rand() % co_group_cnt + 1;
+            //printf("next_co_id:%d\n", next_co_id);
+            next_co = &co_main;
+            while (--next_co_id)
+            {
+                next_co = next_co->next;
+            }
+            //printf("next_co->status:%d\n", next_co->status);
+            //printf("co_group_cnt:%d\n", co_group_cnt);
         } while (next_co->status != CO_RUNNING && next_co->status != CO_NEW);
 
-        assert(next_co != NULL);
         //printf("switch to: %s %d\n", next_co->name, next_co->status);
         co_current = next_co;
         switch (co_current->status)
         {
         case CO_NEW:
-            stack_switch_call((void *)((uintptr_t)co_current->stack + STACK_SIZE - sizeof(uintptr_t)), coroutine_entry, (uintptr_t)co_current);
-            //stack_switch_call((void *)((((uintptr_t)co_current->stack >> 4) << 4) + STACK_SIZE), coroutine_entry, (uintptr_t)co_current);
+            stack_switch_call((void *)(co_current->stack + STACK_SIZE), coroutine_entry, (uintptr_t)co_current);
             //puts("out");
             break;
         case CO_RUNNING:
@@ -141,20 +141,18 @@ void co_yield()
         default:
             //printf("%s %d\n", co_current->name, co_current->status);
             assert(0);
-            break;
         }
     }
 }
 
 void __attribute__((constructor)) co_init()
 {
-    co_group[0].name = "main"; // main will be always waiting for other routines
-    co_group[0].status = CO_RUNNING;
-    co_current = &co_group[0];
+    co_main.name = "main"; // main will be always waiting for other routines
+    co_main.status = CO_RUNNING;
     co_group_cnt = 1;
 }
 
 void __attribute__((destructor)) co_end()
 {
-    printf("co_group_cnt:%d\n", co_group_cnt);
+    //printf("co_group_cnt:%d\n", co_group_cnt);
 }
