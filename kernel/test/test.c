55df3c97be932afb739d66a9b4fbcb14afa950f4
tracer-nju <tracer@nju.edu.cn>
2021-04-07 14:42:47 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 109302dec4e540f9a9ee71c76cacd8c4   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  14:42:47 up  2:15,  0 users,  load average: 0.02, 0.05, 0.01

---
+++ kernel/test/test.c
@@ -0,0 +1,9 @@
+static void entry(int tid) { pmm->alloc(128); }
+static void goodbye() { printf("End.\n"); }
+int main()
+{
+    pmm->init();
+    for (int i = 0; i < 4; i++)
+        create(entry);
+    join(goodbye);
+}
