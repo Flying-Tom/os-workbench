61989dcfd38ceece8cc8f7a5acbdf148ea071f56
tracer-nju <tracer@nju.edu.cn>
2021-04-08 19:17:08 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 73d54c83f7834d2d80fa8982b75ba418   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  19:17:08 up  9:19,  0 users,  load average: 0.04, 0.08, 0.03

--- kernel/framework/kernel.h
+++ kernel/framework/kernel.h
@@ -1,4 +1,5 @@
 #include <am.h>
+#include <stddef.h>
 
 #define MODULE(mod) \
   typedef struct mod_##mod##_t mod_##mod##_t; \
--- kernel/test/common.h
+++ kernel/test/common.h
@@ -1,6 +1,5 @@
 #include <kernel.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <stdio.h>
 
 /*
