b41bb75a65d6a7d46b845ec6a0dbd18987cced35
tracer-nju <tracer@nju.edu.cn>
2021-04-08 10:35:58 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 73d54c83f7834d2d80fa8982b75ba418   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  10:35:58 up 38 min,  0 users,  load average: 0.01, 0.04, 0.01

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -27,11 +27,21 @@ static void kfree(void *ptr)
 {
 }
 
+#ifndef TEST
 static void pmm_init()
 {
     uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
     printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
+#else
+static void pmm_init()
+{
+    char *ptr = malloc(HEAP_SIZE);
+    heap.start = ptr;
+    heap.end = ptr + HEAP_SIZE;
+    printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+}
+#endif
 
 MODULE_DEF(pmm) = {
     .init = pmm_init,
