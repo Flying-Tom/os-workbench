c5a362653a243541bf0db9374aa815fee8f0655a
tracer-nju <tracer@nju.edu.cn>
2021-04-10 17:38:02 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: a89a21c3335b477196aefd648e28d3e9   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  17:38:02 up  3:54,  0 users,  load average: 0.38, 0.39, 0.17

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -49,6 +49,7 @@ static void *kalloc(size_t size)
             cur->size = cur->size - size - sizeof(node_t);
             new_node->next = cur->next;
             cur->next = new_node;
+            printf("ret:%p", new_node);
             return new_node;
         }
     }
