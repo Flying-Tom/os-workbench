a826a012f2773085f3f014599db8c8a12480e0ba
tracer-nju <tracer@nju.edu.cn>
2021-04-12 20:20:19 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 974e6a4d94ca416893ea910c5a2ce7cb   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:20:19 up 10:35,  0 users,  load average: 0.01, 0.07, 0.08

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -6,12 +6,12 @@
 #define MB *(1L << 20)
 #define GB *(1L << 30)
 
-#define Log(format, ...)                           \
-    _Log("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
+#define _Log(format, ...)                           \
+    printf("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
          __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
 #ifdef DEBUG
-#define Log(...) Log(__VA_ARGS__)
+#define Log(...) _Log(__VA_ARGS__)
 #else
 #define Log(...)
 #endif
