fce1e85b8533005b049b45bf99697390cac737f0
tracer-nju <tracer@nju.edu.cn>
2021-04-12 19:00:09 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 974e6a4d94ca416893ea910c5a2ce7cb   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  19:00:09 up  9:15,  0 users,  load average: 0.10, 0.04, 0.01

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -78,7 +78,7 @@ static void *slab_alloc(size_t size)
         object_slab_list = slab_list[cpu_id][slab_type] = get_one_page(size);
     }
     printf("page_head(1):%p\n", PAGE_HEADER(1));
-    printf("object_slab_list:%p\n", object_slab_list);
+    printf("object_slab_list:%p\n", (uintptr_t)object_slab_list - (PAGE_SIZE - sizeof(page_header)) - PAGE_SIZE);
     printf("(uintptr_t *)object_slab_list - (PAGE_SIZE - sizeof(page_header)):%p\n", (uintptr_t *)object_slab_list - (PAGE_SIZE - sizeof(page_header)));
     ret = (void *)((uintptr_t *)object_slab_list - (PAGE_SIZE - sizeof(page_header)) + (slab_type + 1) * 4 * object_slab_list->inode_num);
     object_slab_list->inode_num++;
