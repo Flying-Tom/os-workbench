f6f482dbf35be92499529a6c62ef923d20b364f7
tracer-nju <tracer@nju.edu.cn>
2021-04-12 20:11:04 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 974e6a4d94ca416893ea910c5a2ce7cb   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:11:04 up 10:25,  0 users,  load average: 0.10, 0.06, 0.07

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -96,7 +96,7 @@ static void *slab_alloc(size_t size)
     object_slab_list->inode_num++;
 
     object_slab_list->size -= (1 << (slab_type + 2));
-    assert((uintptr_t)ret % (slab_type << 2) == 0);
+    assert((uintptr_t)ret % (1 << size) == 0);
     return ret;
 }
 
