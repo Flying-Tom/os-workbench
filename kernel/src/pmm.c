eec72562700113d80057deed558f03c40c57505e
tracer-nju <tracer@nju.edu.cn>
2021-04-12 18:56:58 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 974e6a4d94ca416893ea910c5a2ce7cb   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  18:56:58 up  9:11,  0 users,  load average: 0.00, 0.02, 0.00

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -75,12 +75,12 @@ static void *slab_alloc(size_t size)
     {
         if (object_slab_list != NULL)
             printf("object_slab_list->size:%d\n", object_slab_list->size);
-        slab_list[cpu_id][slab_type] = get_one_page(size);
-        object_slab_list = slab_list[cpu_id][slab_type];
+        object_slab_list = slab_list[cpu_id][slab_type] = get_one_page(size);
     }
-    printf("(uintptr_t *)object_slab_list - (PAGE_SIZE - sizeof(page_header)):%p\n",(uintptr_t *)object_slab_list - (PAGE_SIZE - sizeof(page_header)));
+    printf("(uintptr_t *)object_slab_list - (PAGE_SIZE - sizeof(page_header)):%p\n", (uintptr_t *)object_slab_list - (PAGE_SIZE - sizeof(page_header)));
     ret = (void *)((uintptr_t *)object_slab_list - (PAGE_SIZE - sizeof(page_header)) + (slab_type + 1) * 4 * object_slab_list->inode_num);
     object_slab_list->inode_num++;
+    printf("object_slab_list->inode_num:%d\n",object_slab_list->inode_num);
     object_slab_list->size -= (slab_type + 1) * 4;
     assert((uintptr_t)ret % (1 << size) == 0);
     return ret;
