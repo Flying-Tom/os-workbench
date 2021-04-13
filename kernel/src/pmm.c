1e3567f513966d72b0c6ff7f5928106073dc624f
tracer-nju <tracer@nju.edu.cn>
2021-04-13 16:43:58 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 0d27c8e4c0dd44a7bd55f3d5bc8086f4   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  16:43:58 up  3:52,  0 users,  load average: 0.02, 0.05, 0.00

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -117,14 +117,10 @@ static void *slab_alloc(size_t size)
         object_cache->newest_slab = get_one_page(size);
     }
 
-    ret = (void *)((uintptr_t *)object_cache->newest_slab - (PAGE_SIZE - sizeof(page_header)) + object_cache->newest_slab->size);
+    ret = (void *)((uint8_t *)object_cache->newest_slab - (PAGE_SIZE - sizeof(page_header)) + object_cache->newest_slab->size);
     Log("object_cache->newest_slab->size:%d", object_cache->newest_slab->size);
     object_cache->newest_slab->size += size;
 
-    Log("object_cache->newest_slab :%p", object_cache->newest_slab);
-    Log("PAGE_SIZE - sizeof(page_header) :%p", PAGE_SIZE - sizeof(page_header));
-    Log("???? :%p", (char *)object_cache->newest_slab - (0 ));
-    Log("???? :%p", (char *)object_cache->newest_slab - (1 ));
     /*
     Log("alloc size:%d", size);
     Log("minium power 2:%d", poweraligned(size));
