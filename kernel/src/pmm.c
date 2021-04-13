b99bbc390bd569506db53258fc96040405f9ef03
tracer-nju <tracer@nju.edu.cn>
2021-04-13 21:37:30 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 0d27c8e4c0dd44a7bd55f3d5bc8086f4   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  21:37:30 up  8:45,  0 users,  load average: 0.00, 0.04, 0.00

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -125,7 +125,9 @@ static void *kalloc(size_t size)
         return NULL;
     if (size > 512)
     {
+        lock(&lk);
         ret = buddy_alloc(size);
+        unlock(&lk);
     }
     else
         ret = slab_alloc(size);
