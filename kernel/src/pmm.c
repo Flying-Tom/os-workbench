daec4757af925852efe2faec8295f59c27c02916
tracer-nju <tracer@nju.edu.cn>
2021-04-13 16:32:23 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 0d27c8e4c0dd44a7bd55f3d5bc8086f4   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  16:32:23 up  3:40,  0 users,  load average: 0.04, 0.05, 0.01

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -121,7 +121,7 @@ static void *slab_alloc(size_t size)
     Log("object_cache->newest_slab->size:%d", object_cache->newest_slab->size);
     object_cache->newest_slab->size += size;
 
-    Log("(uintptr_t *)object_cache->newest_slab - (PAGE_SIZE - sizeof(page_header)):%p", (uintptr_t *)object_cache->newest_slab - (PAGE_SIZE - sizeof(page_header)));
+    Log("(uintptr_t *)object_cache->newest_slab :%p", (uintptr_t *)object_cache->newest_slab );
     /*
     Log("alloc size:%d", size);
     Log("minium power 2:%d", poweraligned(size));
