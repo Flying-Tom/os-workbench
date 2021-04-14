9c3dbd2c110f80b24a67956b9ee85fe92c22dcb9
tracer-nju <tracer@nju.edu.cn>
2021-04-14 20:20:54 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 8e4749cccdfb42d084f43d07b23c665d   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:20:54 up 10:24,  0 users,  load average: 0.05, 0.08, 0.06

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -174,7 +174,7 @@ static void pmm_init()
         cur->next = NULL;
     }
     page_header *cur = PAGE_HEADER(0);
-    assert(cur->next == NULL);
+    assert(cur->next != NULL);
 
     free_list[max_order] = PAGE_HEADER(0);
     get_one_block(14);
