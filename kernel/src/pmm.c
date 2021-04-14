0fc032f577a4c04e37f65b48dbb7e57f25b0f208
tracer-nju <tracer@nju.edu.cn>
2021-04-14 20:28:51 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 8e4749cccdfb42d084f43d07b23c665d   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:28:51 up 10:32,  0 users,  load average: 0.06, 0.08, 0.07

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -186,13 +186,13 @@ static void pmm_init()
     free_list[max_order] = PAGE_HEADER(0);
 
     get_one_block(1);
+    get_one_block(1);
     for (int i = max_order; i >= 1; i--)
     {
         if (free_list[i] != NULL)
             printf("free_list[%d] id:%d \n", i, free_list[i]->id);
     }
 
-    //Log("free_list[14]->id:%d", free_list[14]->next->id);
     assert(0);
     assert((pm_end - pm_start) % PAGE_SIZE == 0);
     Log("Total pages:%d", total_page_num);
