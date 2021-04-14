dd503993371aece2e62d48ef88ee1e00e81ffac0
tracer-nju <tracer@nju.edu.cn>
2021-04-14 20:11:04 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 8e4749cccdfb42d084f43d07b23c665d   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:11:04 up 10:14,  0 users,  load average: 0.04, 0.03, 0.05

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -70,7 +70,7 @@ static void get_one_block(uint8_t order)
     free_list[order] = newpage;
 
     free_list[order + 1] = free_list[order + 1]->next;
-    assert(free_list[order + 1]);
+    assert(free_list[order + 1]->next != NULL);
     if (free_list[order + 1]->next != NULL)
         Log("free_list[order + 1]->id:%d", free_list[order + 1]->id);
     Log("order:%d", order);
