3dcedc4f55a1e3754dceb8536e163976fbac61b7
tracer-nju <tracer@nju.edu.cn>
2021-04-14 17:32:56 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 8e4749cccdfb42d084f43d07b23c665d   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  17:32:56 up  7:36,  0 users,  load average: 0.19, 0.27, 0.12

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -56,7 +56,7 @@ static uint8_t cache_type(size_t size)
 
 static void *buddy_alloc(size_t size)
 {
-    //lock(&lk);
+    lock(&lk);
     cpu_id = cpu_current();
     for (int i = 0; i < total_page_num; i++)
     {
@@ -69,7 +69,7 @@ static void *buddy_alloc(size_t size)
                 cur = PAGE_HEADER(j);
                 cur->parent_cpu_id = cpu_id;
             }
-            //unlock(&lk);
+            unlock(&lk);
             return (void *)PAGE(i);
         }
     }
@@ -107,10 +107,10 @@ static void *kalloc(size_t size)
     assert(size > 0);
     if (size >= PAGE_SIZE)
     {
-        lock(&lk);
+        //lock(&lk);
         ret = buddy_alloc(size);
         assert((uintptr_t)ret % size == 0);
-        unlock(&lk);
+        //unlock(&lk);
     }
     else
         ret = slab_alloc(size);
