f4471731a310d51ea856bfa10dad09d1f9b99c5a
tracer-nju <tracer@nju.edu.cn>
2021-04-15 17:56:27 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 65f25998df86478ba4feede8c7148421   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  17:56:27 up  7:59,  0 users,  load average: 0.16, 0.12, 0.04

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -148,6 +148,7 @@ static void *slab_alloc(size_t size)
     {
         Log("Get new page");
         object_cache->slab_free = (page_header *)(buddy_alloc(size) + PAGE_SIZE - sizeof(page_header));
+        Log("fuck");
     }
     size_t i = 0, j = 0;
     while (object_cache->slab_free->bitmap[i])
