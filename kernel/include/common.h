f6b322ddba0d2bd226ce9ad7c3b7ecada138b80d
tracer-nju <tracer@nju.edu.cn>
2021-04-15 18:51:33 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 65f25998df86478ba4feede8c7148421   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  18:51:33 up  8:54,  0 users,  load average: 0.04, 0.07, 0.11

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -8,7 +8,7 @@
 
 #define max(a, b) ((a > b) ? (a) : (b))
 
-#define DEBUG
+//#define DEBUG
 /*
 #define _Log(format, ...)                           \
     printf("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
