0ca80b00ebe3f575fb64323272fad4208434aa61
tracer-nju <tracer@nju.edu.cn>
2021-04-28 10:40:33 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 681d8cad234541ebb98cc8db1929c826   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  10:40:33 up 40 min,  0 users,  load average: 0.12, 0.09, 0.02

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -105,14 +105,15 @@ static void buddy_init()
 
     buddy = (buddy_node *)((uint8_t *)pm_end - (max_buddy_node_num + 2) * sizeof(buddy_node));
     buddy[1].size = pmm_size / PAGE_SIZE;
+    buddy[1].status = BUD_AVAILABLE;
     size_t cur = 1;
     while (2 * cur + 1 < max_buddy_node_num)
     {
         buddy[2 * cur].size = buddy[2 * cur + 1].size = buddy[cur].size / 2;
         cur++;
     }
-    for (int i = 1; i <= max_buddy_node_num; i++)
-        Log("buddy[%d].size = %d", i, buddy[i].size);
+    //for (int i = 1; i <= max_buddy_node_num; i++)
+    //    Log("buddy[%d].size = %d", i, buddy[i].size);
 
     //buddy_node *cur_node = NULL;
 
