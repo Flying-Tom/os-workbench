6efd8d6a44df4f2480eff2f09e85e1074222da42
tracer-nju <tracer@nju.edu.cn>
2021-04-28 11:28:39 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 681d8cad234541ebb98cc8db1929c826   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  11:28:39 up  1:28,  0 users,  load average: 0.01, 0.06, 0.03

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -221,7 +221,7 @@ static void *slab_alloc(size_t size)
     {
         Log("Get new page");
         void *tmp = buddy_alloc(PAGE_SIZE);
-        Log("tmp:%p", tmp);
+        Log("tmp:%p", tmp + PAGE_SIZE - sizeof(page_header));
         object_cache->slab_free = (page_header *)((uint8_t *)tmp + PAGE_SIZE - sizeof(page_header));
         Log("object_cache->slab_free:%p", object_cache->slab_free);
         object_cache->slab_free->parent_cpu_id = cur_cpu_id;
