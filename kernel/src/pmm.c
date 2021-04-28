3d476634451adcc4273cfcea0313d061c107461e
tracer-nju <tracer@nju.edu.cn>
2021-04-28 11:15:05 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 681d8cad234541ebb98cc8db1929c826   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  11:15:05 up  1:14,  0 users,  load average: 0.10, 0.08, 0.02

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -174,7 +174,7 @@ static void *buddy_alloc(size_t size)
     Log("Got buddy node id :%d", obj_buddy_node);
     unlock(&buddy_lk);
 
-    ret = (void *)PAGE(size * obj_buddy_node);
+    ret = (void *)PAGE(size * obj_buddy_node % (1 << log(obj_buddy_node)));
     Log("ret:%p", ret);
     //ret = (void *)PAGE(get_one_buddy_node(order));
     unlock(&pm_global_lk);
