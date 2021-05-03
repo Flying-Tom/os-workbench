16dedc257ab94fa3e831d867582bb8382817c0f7
tracer-nju <tracer@nju.edu.cn>
2021-05-03 20:15:59 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4414392ab38a41a890c5948e45c39bac   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:16:00 up 3 days,  6:07,  0 users,  load average: 0.00, 0.00, 0.00

---
+++ kernel/include/pmm.h
@@ -0,0 +1,70 @@
+#include <common.h>
+#include <lock.h>
+
+#define BREAKPOINT(a) Log("BREAKPOINT:" #a "\n")
+#define align(base, offset) (((base + offset - 1) / offset) * offset) // Right align
+
+/* const macro */
+#define PAGE_SIZE (4 KB)
+#define MAX_CPU_NUM 8
+#define MAX_SLAB_TYPE 12
+
+/* page header */
+#define PAGE_HEADER(a) (page_header *)(pm_start + (a + 1) * PAGE_SIZE - sizeof(page_header))
+#define PAGE(a) (pm_start + a * PAGE_SIZE)
+#define slab_max_items(size) ((PAGE_SIZE - sizeof(page_header)) / size)
+
+/* buddy system*/
+#define MAX_BUDDY_NODE_TYPE 20
+
+/* bitmap */
+#define BITMAP_FULL(page, id) (page->bitmap[id] + 1ULL == 0ULL)
+
+/*------------------------------------------*/
+static lock_t pm_global_lk = LOCK_INIT();
+static lock_t page_lk = LOCK_INIT();
+static lock_t buddy_lk = LOCK_INIT();
+
+static uintptr_t pm_start, pm_end;
+static size_t pmm_size;
+static uint8_t cpu_id, cpu_num;
+static size_t total_page_num;
+static size_t max_buddy_node_num;
+
+typedef struct page_header
+{
+    size_t id;
+    uint8_t parent_cpu_id;
+    uint64_t bitmap[64];
+    uint8_t slab_type;
+    struct page_header *next;
+} page_header;
+
+typedef struct Cache
+{
+    int size;
+    int num_of_slab;
+    page_header *slab_free;
+} Cache;
+Cache cache[MAX_CPU_NUM][8];
+
+enum
+{
+    BUD_EMPTY,
+    BUD_AVAILABLE,
+    BUD_USED,
+    BUD_SPLITTED
+};
+
+typedef struct buddy_node
+{
+    uint8_t status;
+    size_t size;
+} buddy_node;
+
+buddy_node *buddy;
+
+/* buddy system */
+static void buddy_init();
+static size_t get_one_buddy_node(size_t cur, size_t size);
+static void *buddy_alloc(size_t size)
---
+++ kernel/src/pmm/buddy.c
@@ -0,0 +1,83 @@
+#include <pmm.h>
+
+static void buddy_init()
+{
+    Log("heap.start:%p", heap.start);
+    Log("heap.end:%p", heap.end);
+    pm_start = (uintptr_t)heap.start;
+    pm_end = (uintptr_t)heap.end;
+    pmm_size = pm_end - pm_start;
+    Log("max pages:%d", pmm_size / PAGE_SIZE);
+
+    pm_start = align(pm_start, PAGE_SIZE);
+
+    total_page_num = pmm_size / PAGE_SIZE;
+    max_buddy_node_num = 2 * total_page_num;
+    buddy = (buddy_node *)((uint8_t *)pm_end - (max_buddy_node_num + 2) * sizeof(buddy_node));
+    buddy[1].size = pmm_size / PAGE_SIZE;
+    buddy[1].status = BUD_AVAILABLE;
+    size_t cur = 1;
+    while (2 * cur + 1 <= max_buddy_node_num)
+    {
+        buddy[2 * cur].size = buddy[2 * cur + 1].size = buddy[cur].size / 2;
+        cur++;
+    }
+
+    Log("max_buddy_node_num:%d", max_buddy_node_num);
+    Log("total_page_num:%d", total_page_num);
+    Log("pm_start:%p", pm_start);
+    Log("pm_end:%p", pm_end);
+}
+
+static size_t get_one_buddy_node(size_t cur, size_t size)
+{
+    //Log("buddy[%d].status = %d , buddy[%d].size = %d ", cur, buddy[cur].status, cur, buddy[cur].size);
+    if (buddy[cur].status == BUD_AVAILABLE && buddy[cur].size >= size)
+    {
+        if (buddy[cur].size < 2 * size)
+        {
+            buddy[cur].status = BUD_USED;
+            size_t parent = cur / 2;
+            while (buddy[2 * parent].status == BUD_USED && buddy[2 * parent + 1].status == BUD_USED)
+            {
+                buddy[parent].status = BUD_USED;
+                parent /= 2;
+            }
+            return cur;
+        }
+        else
+        {
+            if (buddy[2 * cur].status == BUD_AVAILABLE)
+                return get_one_buddy_node(2 * cur, size);
+            if (buddy[2 * cur + 1].status == BUD_AVAILABLE)
+                return get_one_buddy_node(2 * cur + 1, size);
+
+            //buddy[cur].status = BUD_SPLITTED;
+            buddy[2 * cur].status = buddy[2 * cur + 1].status = BUD_AVAILABLE;
+            return get_one_buddy_node(2 * cur, size);
+        }
+    }
+    Log("Get buddy node failed! cur = %d size = %d", cur, size);
+    assert(0);
+    return 0;
+}
+
+static void *buddy_alloc(size_t size)
+{
+    void *ret = NULL;
+    size_t obj_buddy_node = 0;
+    size = size / PAGE_SIZE;
+    Log("buddy_alloc %d page ", size);
+
+    lock(&buddy_lk);
+    obj_buddy_node = get_one_buddy_node(1, size);
+    Log("Got buddy node id :%d", obj_buddy_node);
+
+    ret = (void *)PAGE(size * (obj_buddy_node % (1 << log(obj_buddy_node))));
+    Log("ret:%p", ret);
+
+    unlock(&buddy_lk);
+
+    assert((uintptr_t)ret % size == 0);
+    return ret;
+}
