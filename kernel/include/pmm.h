dc1288b053cda1206424175a96034dce31d38237
tracer-nju <tracer@nju.edu.cn>
2021-05-03 20:19:31 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4414392ab38a41a890c5948e45c39bac   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:19:31 up 3 days,  6:10,  0 users,  load average: 0.00, 0.00, 0.00

--- kernel/include/pmm.h
+++ kernel/include/pmm.h
@@ -68,3 +68,31 @@ buddy_node *buddy;
 static void buddy_init();
 static size_t get_one_buddy_node(size_t cur, size_t size);
 static void *buddy_alloc(size_t size);
+
+/* Tool func */
+static size_t log(size_t x)
+{
+    size_t ret = 0;
+    while (x > 1)
+    {
+        x >>= 1;
+        ret++;
+    }
+    return ret;
+}
+
+static size_t binalign(size_t size)
+{
+    bool flag = true;
+    size_t ret = 1;
+    assert(size > 0);
+    while (size != 1)
+    {
+        if (size & 1)
+            flag = false;
+        ret <<= 1;
+        size >>= 1;
+    }
+    ret = flag == true ? ret : ret << 1;
+    return ret;
+}
--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,34 +1,5 @@
 #include <pmm.h>
 
-static size_t log(size_t x)
-{
-    size_t ret = 0;
-    while (x > 1)
-    {
-        x >>= 1;
-        ret++;
-    }
-    return ret;
-}
-
-static size_t binalign(size_t size)
-{
-    bool flag = true;
-    size_t ret = 1;
-    assert(size > 0);
-    while (size != 1)
-    {
-        if (size & 1)
-            flag = false;
-        ret <<= 1;
-        size >>= 1;
-    }
-    ret = flag == true ? ret : ret << 1;
-    return ret;
-}
-
-
-
 static bool page_full(page_header *cur)
 {
     if (cur == NULL)
