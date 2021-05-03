4eadbe3d28925945101ecb9f2fe08619cee1dbcc
tracer-nju <tracer@nju.edu.cn>
2021-05-03 23:46:19 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4414392ab38a41a890c5948e45c39bac   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  23:46:19 up 3 days,  9:37,  0 users,  load average: 0.11, 0.05, 0.01

--- kernel/include/pmm.h
+++ kernel/include/pmm.h
@@ -25,7 +25,7 @@ static lock_t pm_global_lk = LOCK_INIT();
 static lock_t page_lk = LOCK_INIT();
 
 static uintptr_t pm_start, pm_end;
-
+static size_t pmm_size;
 static uint8_t cpu_id, cpu_num;
 static size_t total_page_num;
 static size_t max_buddy_node_num;
