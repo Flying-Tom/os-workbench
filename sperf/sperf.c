0a0ed43c9164f4d23d670f8163eecb6a8846b443
tracer-nju <tracer@nju.edu.cn>
2021-05-07 20:32:04 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 30da58e4c44f4db28c59414b8f79fe94   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:32:04 up 12:38,  0 users,  load average: 0.07, 0.06, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     };
     //execve("strace", exec_argv, exec_envp);
     //execve("/bin/strace", exec_argv, exec_envp);
-    //execve("/usr/bin/strace", exec_argv, exec_envp);
+    execve("/usr/bin/strace", exec_argv, exec_envp);
     perror(argv[0]);
     exit(EXIT_FAILURE);
 }
