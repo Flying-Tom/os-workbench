14232f7971f753878504d48523fc647a831b23d1
tracer-nju <tracer@nju.edu.cn>
2021-05-07 20:55:03 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 30da58e4c44f4db28c59414b8f79fe94   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:55:03 up 13:01,  0 users,  load average: 0.07, 0.02, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char *argv[])
 {
+    /*
     char *exec_argv[] = {
         "strace",
         "ls",
@@ -13,9 +14,21 @@ int main(int argc, char *argv[])
         "PATH=/bin",
         NULL,
     };
-    //execve("strace", exec_argv, exec_envp);
+    execve("strace", exec_argv, exec_envp);
     execve("/bin/strace", exec_argv, exec_envp);
-    //execve("/usr/bin/strace", exec_argv, exec_envp);
+    execve("/usr/bin/strace", exec_argv, exec_envp);
     perror(argv[0]);
     exit(EXIT_FAILURE);
+    */
+    pid_t pid;
+    pid = fork();
+    if (pid == 0) //返回子进程
+    {
+        printf("child pid: %d\n", getpid());
+    }
+    else
+    {
+        printf("pid: %d\n", pid); //父进程中返回子进程的pid
+        printf("father pid: %d\n", getpid());
+    }
 }
