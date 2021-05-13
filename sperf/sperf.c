2b13f42ab24ad12838b721deaa6f46e4fa5db599
tracer-nju <tracer@nju.edu.cn>
2021-05-13 15:45:16 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4f1c9eb439824276bb186ff07e3f2a76   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  15:45:16 up 20:03,  0 users,  load average: 0.13, 0.06, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -58,12 +58,14 @@ int main(int argc, char *argv[], char *envp[])
     {
         /* father process */
         dup2(channel[0], STDIN_FILENO);
-
+        /*
         while (fgets(buf, 4096, stdin) != NULL)
         {
             printf("fuck\n");
             printf("%s\n", buf);
         }
+        */
+        getchar();
         printf("Finished!\n");
     }
 }
