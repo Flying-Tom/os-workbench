f7958c623a60516b418c8f3bdef97c01921b5ad1
tracer-nju <tracer@nju.edu.cn>
2021-05-13 15:45:20 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4f1c9eb439824276bb186ff07e3f2a76   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  15:45:20 up 20:03,  0 users,  load average: 0.12, 0.06, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[], char *envp[])
             printf("%s\n", buf);
         }
         */
-        getchar();
+        //getchar();
         printf("Finished!\n");
     }
 }
