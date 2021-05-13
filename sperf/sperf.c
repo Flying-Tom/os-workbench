cb6b19dbf183b44dc945da5f512e874c18bcf9d7
tracer-nju <tracer@nju.edu.cn>
2021-05-13 12:35:09 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4f1c9eb439824276bb186ff07e3f2a76   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  12:35:09 up 16:53,  0 users,  load average: 0.17, 0.14, 0.10

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -32,6 +32,7 @@ int main(int argc, char *argv[])
     {
         /* child process */
         printf("execve(%s, argv, exec_envp)\n", argv[1]);
+        printf("%s\n", argv + 2);
         execve(argv[1], argv + 2, exec_envp);
     }
     else
