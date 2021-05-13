d455a9a5fb31912d99d267117cf509811a7ad27a
tracer-nju <tracer@nju.edu.cn>
2021-05-13 18:10:06 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4f1c9eb439824276bb186ff07e3f2a76   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  18:10:06 up 22:28,  0 users,  load average: 0.00, 0.01, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -19,7 +19,7 @@ void child(int pipe, int exec_argc, char *argv[], char *exec_envp[])
     memcpy(exec_argv + 2, argv + 1, exec_argc * sizeof(char *));
     dup2(pipe, STDOUT_FILENO);
     //dup2(pipe, STDERR_FILENO);
-    printf("111111");
+    printf("fuck you");
     //execve("strace", exec_argv, exec_envp);
 }
 
