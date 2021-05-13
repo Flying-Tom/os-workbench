a1c7c87da41e80bc14249a3bc4956961cc60c3d0
tracer-nju <tracer@nju.edu.cn>
2021-05-13 23:04:32 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4f1c9eb439824276bb186ff07e3f2a76   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  23:04:32 up 1 day,  3:22,  0 users,  load average: 0.06, 0.07, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -49,14 +49,14 @@ void parent(int pipe)
         {
             if (strcmp(syscall_rec[syscall_rec_cnt].name, syscall_name) == 0)
             {
-                printf("%s\n", syscall_rec[syscall_rec_cnt].name);
+                //printf("%s\n", syscall_rec[syscall_rec_cnt].name);
                 syscall_rec[syscall_rec_cnt].time += syscall_time;
                 break;
             }
         }
         if (syscall_rec_cnt > syscall_num)
         {
-            printf("%s\n", syscall_rec[syscall_rec_cnt].name);
+            //printf("%s\n", syscall_rec[syscall_rec_cnt].name);
             syscall_num++;
             memcpy(syscall_rec[syscall_rec_cnt].name, syscall_name, sizeof(syscall_name));
             syscall_rec[syscall_rec_cnt].time = syscall_time;
