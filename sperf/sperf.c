355b0053b11d02b9d2eef89a668501cb99d60912
tracer-nju <tracer@nju.edu.cn>
2021-05-15 01:33:12 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4f1c9eb439824276bb186ff07e3f2a76   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  01:33:12 up 2 days,  5:51,  0 users,  load average: 0.06, 0.04, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -108,10 +108,10 @@ void parent(int pipe)
             {
                 now++;
                 printf("%d\n", syscall_num);
-                /*
+
                 for (int i = 0; i <= syscall_num; i++)
-                    printf("%s(%.0lf%%)\n", syscall_rec[i].name, 100 * syscall_rec[i].time / total_exec_time);
-                */
+                    printf("%d:%s(%.0lf%%)\n", i, syscall_rec[i].name, 100 * syscall_rec[i].time / total_exec_time);
+
                 for (int i = 0; i < 80; i++)
                     printf("%c", '\0');
                 fflush(stdout);
