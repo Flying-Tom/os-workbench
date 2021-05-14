af115c28d7973e481e7b359540b76b8f1f3d68c2
tracer-nju <tracer@nju.edu.cn>
2021-05-14 11:02:36 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4f1c9eb439824276bb186ff07e3f2a76   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  11:02:37 up 1 day, 15:20,  0 users,  load average: 0.09, 0.04, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -58,6 +58,7 @@ void child(int pipe, int exec_argc, char *argv[], char *exec_envp[])
     strcpy(exec_path, temp);
     strcat(exec_path, "/strace");
 
+    printf("%s\n", exec_path);
     while (execve(exec_path, exec_argv, exec_envp) == -1 && temp != NULL)
     {
         printf("%s\n", exec_path);
