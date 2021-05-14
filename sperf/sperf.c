8c63b22c33f04338647c1787e2ae2259dadd06b9
tracer-nju <tracer@nju.edu.cn>
2021-05-14 09:12:54 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 4f1c9eb439824276bb186ff07e3f2a76   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  09:12:54 up 1 day, 13:30,  0 users,  load average: 0.10, 0.07, 0.02

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -44,7 +44,7 @@ void child(int pipe, int exec_argc, char *argv[], char *exec_envp[])
     int trash = open("/dev/null", O_WRONLY);
 
     memcpy(exec_argv + 2, argv + 1, exec_argc * sizeof(char *));
-    dup2(trash, STDOUT_FILENO);
+    //dup2(trash, STDOUT_FILENO);
     //dup2(pipe, STDERR_FILENO);
 
     char exec_path[128];
