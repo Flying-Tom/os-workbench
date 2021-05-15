3ffeb810c04322efb192f23a9f6af2257dcf8d6c
tracer-nju <tracer@nju.edu.cn>
2021-05-15 11:31:49 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: e3aaf3ca9d964aa58a881ffaa5c21ba4   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  11:31:50 up 35 min,  0 users,  load average: 0.16, 0.10, 0.06

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  static char line[4096];
-  while (1) {
-    printf("crepl> ");
-    fflush(stdout);
-    if (!fgets(line, sizeof(line), stdin)) {
-      break;
+int main(int argc, char *argv[])
+{
+    static char line[4096];
+    while (1)
+    {
+        printf("crepl> ");
+        fflush(stdout);
+        if (!fgets(line, sizeof(line), stdin))
+        {
+            break;
+        }
+        printf("Got %zu chars.\n", strlen(line)); // ??
     }
-    printf("Got %zu chars.\n", strlen(line)); // ??
-  }
 }
