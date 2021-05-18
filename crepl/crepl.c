ec650bfd3fad288d05591f68f36f95dea1e0b7fd
tracer-nju <tracer@nju.edu.cn>
2021-05-18 15:20:37 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 1f075e9d9fc940f99fe68a94b4bec294   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  15:20:37 up  2:08,  0 users,  load average: 0.00, 0.00, 0.00

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -6,19 +6,27 @@
 char func_template[] = "/home/flyingtom/os-workbench/crepl/tmp/crepl-XXXXXX";
 int func_cnt = 0;
 int fd;
+char file_path[128] = {'\0'};
 
 void FuncBuild(char buf[])
 {
+    /*
     fd = mkstemp(func_template);
     printf("int func() fd:%d\n", fd);
     printf("fd:%d buf:%s \n", fd, buf);
     write(fd, buf, strlen(buf));
     close(fd);
+    */
 }
 
 int main(int argc, char *argv[])
 {
     static char line[4096];
+    char buf[1024] = {'\0'};
+    fd = mkstemp(func_template);
+    snprintf(buf, sizeof(buf), "/proc/self/fd/%d", fd);
+    readlink(buf, file_path, sizeof(file_path) - 1);
+    printf("%s\n", file_path);
     while (1)
     {
         printf("crepl> ");
