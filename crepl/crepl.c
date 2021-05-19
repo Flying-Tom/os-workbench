0ee0b9bfe8e14afb7f2dfd054af70aac3b4e0d51
tracer-nju <tracer@nju.edu.cn>
2021-05-19 14:51:22 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 220e0acd68554c108fb1d5c3c0753dad   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  14:51:22 up  3:00,  0 users,  load average: 0.03, 0.05, 0.02

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -36,8 +36,8 @@ void Compile()
     else
     {
         close(compile_pipe[1]);
-        read(compile_pipe[0], buf, 256);
         char buf[512];
+        read(compile_pipe[0], buf, 256);
         if (strlen(buf) != 0)
         {
             puts("\033[31mCompile Error\033[0m");
