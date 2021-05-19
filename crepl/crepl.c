550dee3cbb2c2790e3adc989acb248994abc62cb
tracer-nju <tracer@nju.edu.cn>
2021-05-20 00:35:07 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 220e0acd68554c108fb1d5c3c0753dad   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  00:35:07 up 12:43,  0 users,  load average: 0.09, 0.06, 0.01

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -101,7 +101,7 @@ int main(int argc, char *argv[])
                 if (Compile(line, EXPR))
                 {
                     int (*func)(void) = dlsym(handle, "__expr_wrapper__");
-                    printf(" %s = %d\n", line, func());
+                    printf(" = %d\n", line, func());
                     dlclose(handle);
                 }
             }
