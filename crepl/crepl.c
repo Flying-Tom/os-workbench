15cc3bbc14fb72c28cb2e4ad3d089a1c7b76a83b
tracer-nju <tracer@nju.edu.cn>
2021-05-19 20:38:28 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 220e0acd68554c108fb1d5c3c0753dad   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:38:28 up  8:47,  0 users,  load average: 0.04, 0.08, 0.02

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -72,7 +72,7 @@ void ExprCal(char buf[])
     char wrapper[512];
     char wrapper_name[32];
     buf[strlen(buf) - 1] = '\0';
-    sprintf(wrapper_name, "__expr_wrapper_%d", expr_cnt);
+    sprintf(wrapper_name, "__expr_wrapper_%d", expr_cnt++);
     sprintf(wrapper, "int %s(){ return (%s); }", wrapper_name, buf);
     FILE *fp = fopen(src_path, "w");
     fprintf(fp, "%s", wrapper);
