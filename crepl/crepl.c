57b5cb13acbd36eafa7d4be865cac0303fe1a049
tracer-nju <tracer@nju.edu.cn>
2021-05-19 20:52:11 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 220e0acd68554c108fb1d5c3c0753dad   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:52:11 up  9:01,  0 users,  load average: 0.31, 0.09, 0.02

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -81,7 +81,7 @@ void ExprCal(char buf[])
         {
             int (*func)();
             func = dlsym(handle, wrapper_name);
-            printf("%d\n", func());
+            printf(" %s = %d\n", buf, func());
         }
         dlclose(handle);
     }
@@ -98,8 +98,6 @@ int main(int argc, char *argv[])
     readlink(line, src_path, sizeof(src_path) - 1);
     sprintf(line, "/proc/self/fd/%d", fd_so);
     readlink(line, so_path, sizeof(src_path) - 1);
-    //printf("%s\n", src_path);
-    //printf("%s\n", so_path);
     while (1)
     {
         printf("crepl> ");
