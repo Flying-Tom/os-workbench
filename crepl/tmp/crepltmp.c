57057c9c3ea723046b3881050097d2332a727e9b
tracer-nju <tracer@nju.edu.cn>
2021-05-19 14:41:08 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 220e0acd68554c108fb1d5c3c0753dad   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  14:41:08 up  2:49,  0 users,  load average: 0.05, 0.05, 0.03

--- crepl/tmp/crepltmp.c
+++ crepl/tmp/crepltmp.c
@@ -1,2 +1,4 @@
 int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
 int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
