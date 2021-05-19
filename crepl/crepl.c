4b1317c319ed3cc6085b3ea0341870118a0d712c
tracer-nju <tracer@nju.edu.cn>
2021-05-19 20:36:37 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 220e0acd68554c108fb1d5c3c0753dad   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:36:37 up  8:45,  0 users,  load average: 0.20, 0.10, 0.02

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -58,7 +58,7 @@ void FuncBuild(char buf[])
 
     if (Compile())
     {
-        printf("\033[32m  Added:\033[0m %s\n", buf);
+        printf("\033[32m  Added:\033[0m %s", buf);
         void *handle = NULL;
         if ((handle = dlopen(so_path, RTLD_NOW)) != NULL)
         {
