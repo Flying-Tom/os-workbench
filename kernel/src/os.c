64b529cc6c4774adc37e163a1aebabd889d9b4ee
tracer-nju <tracer@nju.edu.cn>
2021-05-23 15:33:19 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 42fdaa44305d4939a94a1bb7adb4a152   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  15:33:19 up  3:34,  0 users,  load average: 0.21, 0.07, 0.02

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -23,7 +23,7 @@ static void os_run()
     int *a = NULL;
     for (int i = 1; i <= 5; i++)
     {
-        a = pmm->alloc(4 MB);
+        a = pmm->alloc(10 MB);
         pmm->free(a);
     }
     /*
