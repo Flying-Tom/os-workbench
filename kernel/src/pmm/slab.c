c77002d773be0948c3061d6676a7aedc9720060d
tracer-nju <tracer@nju.edu.cn>
2021-05-24 20:56:27 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 42fdaa44305d4939a94a1bb7adb4a152   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:56:27 up 1 day,  8:57,  0 users,  load average: 1.00, 0.62, 0.47

--- kernel/src/pmm/slab.c
+++ kernel/src/pmm/slab.c
@@ -8,13 +8,10 @@ uintptr_t slab_start, slab_end, slab_cur;
 static page_header *slab_get_page()
 {
     page_header *ret = NULL;
-    if (slab_cur + PAGE_SIZE / sizeof(uintptr_t) < slab_end)
+    if (slab_cur + PAGE_SIZE < slab_end)
     {
-        printf("slab_cur:%p\n", slab_cur);
-        printf("slab_cur:%p\n", slab_cur + 1);
-        ret = (page_header *)((uint8_t *)slab_cur + PAGE_SIZE - sizeof(page_header));
-        slab_cur += PAGE_SIZE / sizeof(uintptr_t);
-        printf("slab_cur:%p\n", slab_cur);
+        ret = (page_header *)(slab_cur + PAGE_SIZE - sizeof(page_header));
+        slab_cur += PAGE_SIZE;
     }
     return ret;
 }
