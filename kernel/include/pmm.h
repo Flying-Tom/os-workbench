f903812557240009be1f6f10cdcd0e0595b6a57b
tracer-nju <tracer@nju.edu.cn>
2021-05-29 21:38:11 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5c72833a91f040f3a45434bd364a1d25   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  21:38:11 up  9:13,  0 users,  load average: 2.00, 2.00, 2.00

--- kernel/include/pmm.h
+++ kernel/include/pmm.h
@@ -54,7 +54,7 @@ typedef struct buddy_node
 
 /* slab system */
 void *slab_start, *slab_end;
-void slab_init(int cpu, void *start, size_t size);
+void slab_init(uint8_t cpu, void *start, size_t size);
 void *slab_alloc(size_t size);
 void slab_free(void *ptr);
 
--- kernel/src/pmm/slab.c
+++ kernel/src/pmm/slab.c
@@ -7,7 +7,7 @@ size_t slab_type[MAX_SLAB_TYPE + 1] = {32, 64, 128, 256, 512, 1024, 4096};
 
 static void cache_init(void *start, size_t size, uint8_t type);
 
-static void *slab_get_page()
+static void *slab_pagealloc()
 {
     cpu_id = cpu_id;
     lock(&page_lk[cpu_id]);
@@ -22,13 +22,27 @@ static void *slab_get_page()
     return ret;
 }
 
+static void slab_page_free(void *ptr, uint8_t cpu)
+{
+    if (ptr < slab_start || ptr >= slab_end)
+    {
+        buddy_free(ptr);
+        return;
+    }
+
+    lock(&page_lk[cpu]);
+    *(void **)ptr = page_entry[cpu];
+    page_entry[cpu] = ptr;
+    unlock(page_lk[cpu]);
+}
+
 void *slab_alloc(size_t size)
 {
     cpu_id = CPU_CUR;
     Log("slab alloc %d bytes", size);
     void *ret = NULL;
     if (size > slab_type[MAX_SLAB_TYPE - 1])
-        return slab_get_page();
+        return slab_pagealloc();
     else
     {
         uint8_t i = 0;
@@ -40,7 +54,7 @@ void *slab_alloc(size_t size)
         lock(&cache_lk[cpu_id][i]);
         if (cache_entry[cpu_id][i] == NULL)
         {
-            cache_entry[cpu_id][i] = slab_get_page();
+            cache_entry[cpu_id][i] = slab_pagealloc();
             if (cache_entry[cpu_id][i] == NULL)
             {
                 unlock(&cache_lk[cpu_id][i]);
@@ -53,7 +67,7 @@ void *slab_alloc(size_t size)
 
         ret = cur_page->entry;
         cur_page->entry = *(void **)ret;
-        cur_page->units_remaining -= 1;
+        cur_page->units_remaining--;
         if (cur_page->units_remaining == 0)
         {
             if (cur_page->next != NULL)
@@ -67,6 +81,41 @@ void *slab_alloc(size_t size)
 
 void slab_free(void *ptr)
 {
+    cpu_id = CPU_CUR;
+    if ((uintptr_t)ptr & PAGE_RMASK)
+    {
+        page_header *cur_page(void *)((uintptr_t)ptr & PAGE_MASK);
+
+        lock(&cache_lk[cur_page->cpu][cur_page->type]);
+        *(void **)ptr = cur_page->entry;
+        cur_page->entry = ptr;
+        cur_page->units_remaining++;
+        if (cur_page->units_remaining == 1)
+        {
+            cur_page->next = cache_entry[cur_page->cpu][cur_page->type];
+            if (cur_page->next)
+                ((page_header *)cur_page->next)->prev = cur_page;
+            cur_page->prev = NULL;
+            cache_entry[cur_page->cpu][cur_page->type] = cur_page;
+        }
+        else
+        {
+            if (cur_page->units_remaining + 1 == (PAGE_SIZE - sizeof(page_header) / slab_type[cur_page->type]))
+            {
+                if (cur_page->prev)
+                    ((page_header *)cur_page->prev)->next = cur_page->next;
+                else
+                    cache_entry[cur_page->cpu][cur_page->type] = cur_page->next;
+
+                if (cur_page->next)
+                    ((page_header *)cur_page->next)->prev = cur_page->prev;
+                slab_pagefree(cur_page, cur_page->cpu);
+            }
+            unlock(&cache_lk[cur_page->cpu][cur_page->type]);
+        }
+    }
+    else
+        slab_pagefree(cur_page, cpu_id);
 }
 
 static void cache_init(void *start, size_t size, uint8_t type)
@@ -92,7 +141,7 @@ static void cache_init(void *start, size_t size, uint8_t type)
     *((void **)pm_cur) = NULL;
 }
 
-void slab_init(int cpu, void *start, size_t size)
+void slab_init(uint8_t cpu, void *start, size_t size)
 {
     Log("slab system starts from %p", start);
 
