75c6e190488e7da73dbec34fb98b015cff0de473
tracer-nju <tracer@nju.edu.cn>
2021-05-29 17:08:26 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5c72833a91f040f3a45434bd364a1d25   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  17:08:26 up  4:43,  0 users,  load average: 0.02, 0.01, 0.00

--- kernel/src/pmm/slab.c
+++ kernel/src/pmm/slab.c
@@ -3,7 +3,7 @@
 int cache_lk[MAX_CPU_NUM][MAX_SLAB_TYPE], page_lk[MAX_CPU_NUM];
 void *cache_entry[MAX_CPU_NUM][MAX_SLAB_TYPE], *page_entry[MAX_CPU_NUM];
 
-size_t slab_type[MAX_SLAB_TYPE + 1] = { 32, 64, 128, 256, 512, 1024, 4096};
+size_t slab_type[MAX_SLAB_TYPE + 1] = {32, 64, 128, 256, 512, 1024, 4096};
 
 void cache_init(void *start, size_t size, uint8_t type)
 {
@@ -48,36 +48,41 @@ void *slab_alloc(size_t size)
 {
     Log("slab alloc %d bytes", size);
     void *ret = NULL;
-    int i = 0;
-    while (slab_type[i] < size)
-        i++;
-
-    Log("Slab type:%d", slab_type[i]);
-
-    lock(&cache_lk[CPU_CUR][i]);
-    if (cache_entry[CPU_CUR][i] == NULL)
+    if (size > slab_type[MAX_SLAB_TYPE - 1])
+        return slab_get_page();
+    else
     {
-        cache_entry[CPU_CUR][i] = slab_get_page();
+        int i = 0;
+        while (slab_type[i] < size)
+            i++;
+
+        Log("Slab type:%d", slab_type[i]);
+
+        lock(&cache_lk[CPU_CUR][i]);
         if (cache_entry[CPU_CUR][i] == NULL)
         {
-            unlock(&cache_lk[CPU_CUR][i]);
-            return NULL;
+            cache_entry[CPU_CUR][i] = slab_get_page();
+            if (cache_entry[CPU_CUR][i] == NULL)
+            {
+                unlock(&cache_lk[CPU_CUR][i]);
+                return NULL;
+            }
+            cache_init(cache_entry[CPU_CUR][i], PAGE_SIZE, (uint8_t)i);
         }
-        cache_init(cache_entry[CPU_CUR][i], PAGE_SIZE, (uint8_t)i);
-    }
 
-    page_header *cur_page = cache_entry[CPU_CUR][i];
+        page_header *cur_page = cache_entry[CPU_CUR][i];
 
-    ret = cur_page->entry;
-    cur_page->entry = *(void **)ret;
-    cur_page->units_remaining -= 1;
-    if (cur_page->units_remaining == 0)
-    {
-        if (cur_page->next != NULL)
-            ((page_header *)cur_page->next)->prev = NULL;
-        cache_entry[CPU_CUR][i] = cur_page->next;
+        ret = cur_page->entry;
+        cur_page->entry = *(void **)ret;
+        cur_page->units_remaining -= 1;
+        if (cur_page->units_remaining == 0)
+        {
+            if (cur_page->next != NULL)
+                ((page_header *)cur_page->next)->prev = NULL;
+            cache_entry[CPU_CUR][i] = cur_page->next;
+        }
+        unlock(&cache_lk[CPU_CUR][i]);
     }
-    unlock(&cache_lk[CPU_CUR][i]);
     return ret;
 }
 
