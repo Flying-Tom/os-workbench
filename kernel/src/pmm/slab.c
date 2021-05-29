c34a560d9d8445cee2b3425b524e2bb1eb5051c9
tracer-nju <tracer@nju.edu.cn>
2021-05-29 16:34:25 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5c72833a91f040f3a45434bd364a1d25   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  16:34:25 up  4:09,  0 users,  load average: 0.00, 0.00, 0.00

--- kernel/src/pmm/slab.c
+++ kernel/src/pmm/slab.c
@@ -7,8 +7,8 @@ size_t slab_type[MAX_SLAB_TYPE + 1] = {8, 16, 32, 64, 128, 256, 512, 1024, 2048,
 
 void cache_init(void *start, size_t size, uint8_t type)
 {
-    int unit_size = slab_type[type];
-    int unit_max_num = size / unit_size - 1;
+    size_t unit_size = slab_type[type];
+    size_t unit_max_num = size / unit_size - 1;
     *((page_header *)start) = (page_header){
         .prev = NULL,
         .next = (type == MAX_SLAB_TYPE ? start + unit_size : NULL),
