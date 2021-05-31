eecf3c1d9b7fff94161bc850672cd847fe151698
tracer-nju <tracer@nju.edu.cn>
2021-05-31 13:42:56 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 74425179a2534336aa9c51a3dedb0afc   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  13:42:56 up 20 min,  0 users,  load average: 0.04, 0.05, 0.05

--- kernel/include/pmm.h
+++ kernel/include/pmm.h
@@ -1,31 +1,16 @@
 #include <common.h>
 
-#define BREAKPOINT(a) Log("BREAKPOINT:" #a "\n")
-#define align(base, offset) (((base + offset - 1) / offset) * offset) // Right align
-
 /* const macro */
+#define MAX_CPU_NUM 8
+#define CPU_CUR (uint8_t)(cpu_current())
+#define CPU_NUM (uint8_t)(cpu_count())
+
+/* slab system */
 #define PAGE_SIZE (4 KB)
 #define PAGE_LMASK ~(uintptr_t)(PAGE_SIZE - 1)
 #define PAGE_RMASK (uintptr_t)(PAGE_SIZE - 1)
 #define PAGE_ORDER 12
-#define MAX_CPU_NUM 8
 #define MAX_SLAB_TYPE 6
-#define CPU_CUR (uint8_t)(cpu_current())
-#define CPU_NUM (uint8_t)(cpu_count())
-
-/* page header */
-#define PAGE_HEADER(a) (page_header *)(pm_start + (a + 1) * PAGE_SIZE - sizeof(page_header))
-#define PAGE(a) (pm_start + a * PAGE_SIZE)
-#define slab_max_items(size) ((PAGE_SIZE - sizeof(page_header)) / size)
-
-/* buddy system*/
-
-#define MAX_BUDDY_NODE_TYPE 20
-#define MAX_BUD_ORDER 24
-#define MAX_BUD_SIZE (1 << MAX_BUD_ORDER)
-#define MAX_BUD_LMASK ~(uintptr_t)(MAX_BUD_SIZE - 1)
-
-/*------------------------------------------*/
 
 typedef struct page_header
 {
@@ -37,32 +22,38 @@ typedef struct page_header
     short units_remaining;
 } page_header;
 
-enum
-{
-    BUD_EMPTY,
-    BUD_SPLITTED,
-    BUD_FULL
-};
-
-typedef struct buddy_node
-{
-    void *addr;
-    uint8_t status;
-    uint8_t order;
-} buddy_node;
-
-/* slab system */
 void *slab_start, *slab_end;
 void slab_init(uint8_t cpu, void *start, size_t size);
 void *slab_alloc(size_t size);
 void slab_free(void *ptr);
 
 /* buddy system */
+#define MAX_BUDDY_NODE_TYPE 20
+#define MAX_BUD_ORDER 24
+#define MAX_BUD_SIZE (1 << MAX_BUD_ORDER)
+#define MAX_BUD_LMASK ~(uintptr_t)(MAX_BUD_SIZE - 1)
+
+typedef struct buddy_node
+{
+    void *addr;
+    enum
+    {
+        BUD_EMPTY,
+        BUD_SPLITTED,
+        BUD_FULL
+    };
+    uint8_t status;
+    uint8_t order;
+} buddy_node;
+
 void buddy_init(void *start, void *end);
 void *buddy_alloc(size_t size);
 void buddy_free(void *ptr);
 
-/* Tool func */
+/* Tool func and macro */
 
 uint8_t log(size_t x);
 size_t binalign(size_t size);
+
+#define BREAKPOINT(a) Log("BREAKPOINT:" #a "\n")
+#define align(base, offset) (((base + offset - 1) / offset) * offset) // Right align
