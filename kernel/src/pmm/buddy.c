9c6871b6e952df481c2649cf88dd78da544ccd7c
tracer-nju <tracer@nju.edu.cn>
2021-05-31 13:25:00 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 74425179a2534336aa9c51a3dedb0afc   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  13:25:00 up 2 min,  0 users,  load average: 0.27, 0.18, 0.07

--- kernel/src/pmm/buddy.c
+++ kernel/src/pmm/buddy.c
@@ -33,7 +33,7 @@ static inline void *buddy_alloc_search(int id, uint8_t cur_order, uint8_t tar_or
 
 static inline void buddy_free_search(int id, uint8_t cur_order, void *tar_ptr)
 {
-    //assert(cur_order >= PAGE_ORDER);
+    assert(cur_order >= PAGE_ORDER);
     if (buddy[id].addr == tar_ptr && buddy[id].status == BUD_FULL)
     {
         buddy[id].order = cur_order;
