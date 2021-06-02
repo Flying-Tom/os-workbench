7be3601da6b31b2abcf43834a1dcf91d4341c00c
tracer-nju <tracer@nju.edu.cn>
2021-06-02 21:51:15 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 457c9c27df8f45ab8225e0b56b7803ee   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  21:51:16 up  8:22,  0 users,  load average: 0.14, 0.13, 0.07

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -8,8 +8,25 @@ typedef struct fat_header
     uint32_t BPB_BytsPerSec : 16;
     uint32_t BPB_SecPerClus : 8;
     uint32_t BPB_RsvdSecCnt : 16;
+    uint8_t BPB_NumFATs;
+    uint16_t BPB_RootEntCnt;
+    uint16_t BPB_RootEntCnt;
+    uint8_t BPB_Media;
+    uint32_t BPB_FATSz32;
+    uint16_t BPB_ExtFlags;
+    uint16_t BPB_FSVer;
+    uint32_t BPB_RootClus;
+    uint16_t BPB_FSInfo;
+    uint16_t BPB_BkBootSec;
+    uint8_t BPB_Reserved[12];
+    uint8_t BS_DrvNum;
+    uint8_t BS_Reserved1;
+    uint8_t BS_BootSig;
+    uint32_t BS_VolID;
+    uint8_t BS_VolLab[11];
+    uint8_t BS_VolLab[8];
     uint8_t padding[420];
-    uint16_t signature;
+    uint16_t Signature_word;
 } __attribute__((packed)) fat_header;
 
 int main(int argc, char *argv[])
