dc518ed92cd9ee47071839af8bfe3145d4fac338
tracer-nju <tracer@nju.edu.cn>
2021-06-02 21:40:29 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 457c9c27df8f45ab8225e0b56b7803ee   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  21:40:29 up  8:11,  0 users,  load average: 0.01, 0.04, 0.00

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,2 +1,16 @@
-int main(int argc, char *argv[]) {
+
+struct fat_header
+{
+    uint8_t BS_jmpBoot[3];
+    uint8_t BS_OEMName[8];
+    uint32_t BPB_BytsPerSec : 16;
+    uint32_t BPB_SecPerClus : 8;
+    uint32_t BPB_RsvdSecCnt : 16;
+    uint8_t padding[420];
+    uint16_t signature;
+} __attribute__((packed));
+
+int main(int argc, char *argv[])
+{
+    assert(sizeof(fat_header) == 512);
 }
