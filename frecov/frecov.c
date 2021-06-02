b4a69109932eb8361c4aa2aae1ea40b36dab6c42
tracer-nju <tracer@nju.edu.cn>
2021-06-02 21:41:21 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 457c9c27df8f45ab8225e0b56b7803ee   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  21:41:21 up  8:12,  0 users,  load average: 0.36, 0.13, 0.03

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,6 +1,6 @@
 #include <assert.h>
 
-struct fat_header
+typedef struct fat_header
 {
     uint8_t BS_jmpBoot[3];
     uint8_t BS_OEMName[8];
@@ -9,7 +9,7 @@ struct fat_header
     uint32_t BPB_RsvdSecCnt : 16;
     uint8_t padding[420];
     uint16_t signature;
-} __attribute__((packed));
+} __attribute__((packed)) fat_header;
 
 int main(int argc, char *argv[])
 {
