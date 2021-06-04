fb5af3dba7750fe06e91688e45e87f6ac41dda0a
tracer-nju <tracer@nju.edu.cn>
2021-06-04 21:08:31 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 76e66d9c1f9c4340839d1d0b72af7921   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  21:08:31 up 12:26,  1 user,  load average: 0.07, 0.02, 0.00

--- frecov/frecov.h
+++ frecov/frecov.h
@@ -8,7 +8,7 @@
            __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
 #define _panic(cond, format, ...)                                                                      \
-    if (!cond)                                                                                         \
+    if (!(cond))                                                                                       \
     {                                                                                                  \
         printf("\33[1;31m[%s,%d,%s] " format "\33[0m\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
         assert(cond);                                                                                  \
