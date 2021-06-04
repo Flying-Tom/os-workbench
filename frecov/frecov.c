448ca5234e2a7c0e5d44b546ebdbf144fec8b5ae
tracer-nju <tracer@nju.edu.cn>
2021-06-05 00:55:05 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 76e66d9c1f9c4340839d1d0b72af7921   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  00:55:05 up 16:12,  1 user,  load average: 0.02, 0.01, 0.00

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,59 +1,5 @@
 #include "frecov.h"
 
-typedef struct fat_header
-{
-    uint8_t BS_jmpBoot[3];        // Intel jump instruction to boot code
-    uint8_t BS_OEMName[8];        // OEM Name String
-    uint32_t BPB_BytsPerSec : 16; // Bytes per sector
-    uint32_t BPB_SecPerClus : 8;  // Sectors per cluster
-    uint32_t BPB_RsvdSecCnt : 16; // Reserved sectors
-    uint8_t BPB_NumFATs;          // Number of FATs
-    uint16_t BPB_RootEntCnt;      // Root Entry Count
-    uint16_t BPB_TotSec16;        // 16-bit Count of sectors
-    uint8_t BPB_Media;            // Media Type ID
-    uint16_t BPB_FATSz16;         // Sectors per FAT
-    uint16_t BPB_SecPerTrk;       // Sectors per track
-    uint16_t BPB_NumHeads;        // Number of heads
-    uint32_t BPB_HiddSec;         // Number of hidden sectors
-    uint32_t BPB_TotSec32;        // 32-bit Count of sectors
-    uint32_t BPB_FATSz32;         // 32-bit Count of sectors
-    uint16_t BPB_ExtFlags;        // Extended flags
-    uint16_t BPB_FSVer;           // Extended flags
-    uint32_t BPB_RootClus;        // Root directory start cluster
-    uint16_t BPB_FSInfo;          // File system information sector
-    uint16_t BPB_BkBootSec;       // File system information sector
-    uint8_t BPB_Reserved[12];     // Reserved
-    uint8_t BS_DrvNum;            // Physical drive Number
-    uint8_t BS_Reserved1;         // Reserved
-    uint8_t BS_BootSig;           // Extended boot signature
-    uint32_t BS_VolID;            // Extended boot signature
-    uint8_t BS_VolLab[11];        // Volume label
-    uint8_t BS_FilSysType[8];     // Informational FS Type
-    uint8_t padding[420];         // Reserved
-    uint16_t Signature_word;      // Signature
-} __attribute__((packed)) fat_header;
-
-typedef struct bmp_header
-{
-    uint16_t bfType;
-    uint32_t bfSize;
-    uint32_t bfReserved;
-    uint32_t bfOffBits;
-
-    uint32_t biSize;
-    uint32_t biWidth;
-    uint32_t biHeight;
-    uint16_t biPlanes;
-    uint16_t biBitCount;
-    uint32_t biCompression;
-    uint32_t biSizeImage;
-    uint32_t biXPelsPerMeter;
-    uint32_t biYPelsPerMeter;
-    uint32_t biClrUsed;
-    uint32_t biClrImportant;
-
-} __attribute__((packed)) bmp_header;
-
 int main(int argc, char *argv[])
 {
     panic(sizeof(fat_header) == 512, "bad fat_header");
@@ -70,6 +16,5 @@ int main(int argc, char *argv[])
     panic(strncmp((char *)disk->BS_FilSysType, "FAT32", 5) == 0, "FAT header Error | BS_FilSysTypem : %s", (char *)disk->BS_FilSysType);
     panic(disk->Signature_word == 0xaa55, "FAT header Error | Signature_word : %x ", disk->Signature_word);
 
-    printf("%x\n", disk->BS_jmpBoot[0]);
     fclose(fp);
 }
--- frecov/frecov.h
+++ frecov/frecov.h
@@ -24,3 +24,57 @@
 #define Log(...)
 #define panic(...)
 #endif
+
+typedef struct fat_header
+{
+    uint8_t BS_jmpBoot[3];        // Intel jump instruction to boot code
+    uint8_t BS_OEMName[8];        // OEM Name String
+    uint32_t BPB_BytsPerSec : 16; // Bytes per sector
+    uint32_t BPB_SecPerClus : 8;  // Sectors per cluster
+    uint32_t BPB_RsvdSecCnt : 16; // Reserved sectors
+    uint8_t BPB_NumFATs;          // Number of FATs
+    uint16_t BPB_RootEntCnt;      // Root Entry Count
+    uint16_t BPB_TotSec16;        // 16-bit Count of sectors
+    uint8_t BPB_Media;            // Media Type ID
+    uint16_t BPB_FATSz16;         // Sectors per FAT
+    uint16_t BPB_SecPerTrk;       // Sectors per track
+    uint16_t BPB_NumHeads;        // Number of heads
+    uint32_t BPB_HiddSec;         // Number of hidden sectors
+    uint32_t BPB_TotSec32;        // 32-bit Count of sectors
+    uint32_t BPB_FATSz32;         // 32-bit Count of sectors
+    uint16_t BPB_ExtFlags;        // Extended flags
+    uint16_t BPB_FSVer;           // Extended flags
+    uint32_t BPB_RootClus;        // Root directory start cluster
+    uint16_t BPB_FSInfo;          // File system information sector
+    uint16_t BPB_BkBootSec;       // File system information sector
+    uint8_t BPB_Reserved[12];     // Reserved
+    uint8_t BS_DrvNum;            // Physical drive Number
+    uint8_t BS_Reserved1;         // Reserved
+    uint8_t BS_BootSig;           // Extended boot signature
+    uint32_t BS_VolID;            // Extended boot signature
+    uint8_t BS_VolLab[11];        // Volume label
+    uint8_t BS_FilSysType[8];     // Informational FS Type
+    uint8_t padding[420];         // Reserved
+    uint16_t Signature_word;      // Signature
+} __attribute__((packed)) fat_header;
+
+typedef struct bmp_header
+{
+    uint16_t bfType;
+    uint32_t bfSize;
+    uint32_t bfReserved;
+    uint32_t bfOffBits;
+
+    uint32_t biSize;
+    uint32_t biWidth;
+    uint32_t biHeight;
+    uint16_t biPlanes;
+    uint16_t biBitCount;
+    uint32_t biCompression;
+    uint32_t biSizeImage;
+    uint32_t biXPelsPerMeter;
+    uint32_t biYPelsPerMeter;
+    uint32_t biClrUsed;
+    uint32_t biClrImportant;
+
+} __attribute__((packed)) bmp_header;
