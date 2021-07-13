663fd2814aee66b94b55a8680b501cdb09244957
tracer-nju <tracer@nju.edu.cn>
2021-07-13 18:27:59 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: d3d174fe49e64c148403439686d4cf8e   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  18:27:59 up  6:43,  1 user,  load average: 0.02, 0.12, 0.15

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,10 +1,11 @@
 #include <kernel.h>
 #include <klib.h>
 
-int main() {
-  ioe_init();
-  cte_init(os->trap);
-  os->init();
-  mpe_init(os->run);
-  return 1;
+int main()
+{
+    ioe_init();
+    cte_init(os->trap);
+    os->init();
+    mpe_init(os->run);
+    return 1;
 }
