0931c1b93d372470e2c39666f62800db4f4bed6c
tracer-nju <tracer@nju.edu.cn>
2021-07-16 01:05:55 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7a9aa1dec18847098919a3b892c940a2   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  01:05:55 up 1 day, 15:30,  0 users,  load average: 0.21, 0.28, 0.19

--- kernel/src/kmt/sem.c
+++ kernel/src/kmt/sem.c
@@ -2,15 +2,18 @@
 
 void sem_init(sem_t* sem, const char* name, int value)
 {
+    /*
     sem->value = value;
     kmt->spin_init(&sem->lock, name);
     for (int i = 0; i < MAX_TASK_NUM; i++) {
         sem->tasks[i] = NULL;
     }
     sem->tail = sem->head = 0;
+    */
 }
 void sem_wait(sem_t* sem)
 {
+    /*
     kmt->spin_lock(&sem->lock);
     if (sem->value <= 0) {
         sem->tasks[sem->tail] = cur_task[CPU_CUR];
@@ -24,9 +27,11 @@ void sem_wait(sem_t* sem)
     }
     sem->value--;
     kmt->spin_unlock(&sem->lock);
+    */
 }
 void sem_signal(sem_t* sem)
 {
+    /*
     kmt->spin_lock(&sem->lock);
     sem->value++;
     if (sem->tasks[sem->head] != NULL) {
@@ -34,4 +39,5 @@ void sem_signal(sem_t* sem)
         sem->head = (sem->head + 1) % MAX_TASK_NUM;
     }
     kmt->spin_unlock(&sem->lock);
+    */
 }
