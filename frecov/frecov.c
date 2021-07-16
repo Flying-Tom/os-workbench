a2c2bbd3c8ab17ac94301d6d599a7fe9fecb71a9
tracer-nju <tracer@nju.edu.cn>
2021-07-17 00:07:57 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 83f4889bd6c04e62bc3685aad67170ed   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  00:07:57 up 13:48,  0 users,  load average: 0.06, 0.03, 0.01

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,20 +1,24 @@
 #include "frecov.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     panic(sizeof(fat_header) == 512, "bad fat_header");
     panic(sizeof(bmp_header) == 0x36, "bad bmp_header");
     panic(argc == 2, "Wrong args : argc != 2");
-    char *disk_path = argv[1];
-    FILE *fp = fopen(disk_path, "rb");
+    char* disk_path = argv[1];
+    FILE* fp = fopen(disk_path, "rb");
     panic(fp, "Fail to open the disk :%s", disk_path);
     fseek(fp, 0, SEEK_END);
     uint32_t disk_size = ftell(fp);
     int fd = open(disk_path, O_RDONLY);
 
-    fat_header *disk = mmap(NULL, disk_size, PROT_READ, MAP_SHARED, fd, 0);
-    panic(strncmp((char *)disk->BS_FilSysType, "FAT32", 5) == 0, "FAT header Error | BS_FilSysTypem : %s", (char *)disk->BS_FilSysType);
+    uintptr_t img_addr = (uintptr_t)mmap(NULL, disk_size, PROT_READ, MAP_SHARED, fd, 0);
+    fat_header* disk = (fat_header*)img_addr;
+
+    panic(strncmp((char*)disk->BS_FilSysType, "FAT32", 5) == 0, "FAT header Error | BS_FilSysTypem : %s", (char*)disk->BS_FilSysType);
     panic(disk->Signature_word == 0xaa55, "FAT header Error | Signature_word : %x ", disk->Signature_word);
 
+    void* cluster_addr = (void*)(img_addr + (disk->BPB_RsvdSecCnt + disk->BPB_NumFATs * disk->BPB_FATSz32 + disk->BPB_HiddSec) * disk->BPB_BytsPerSec);
+    close(fd);
     fclose(fp);
 }
