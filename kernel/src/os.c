3b74f773488cbb9dc9de942e635ad6797db28a07
tracer-nju <tracer@nju.edu.cn>
2021-07-16 14:05:12 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 83f4889bd6c04e62bc3685aad67170ed   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  14:05:12 up  3:45,  0 users,  load average: 0.04, 0.04, 0.00

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -31,9 +31,9 @@ static void os_init()
     kmt->sem_init(&empty, "empty", 5); // 缓冲区大小为 5
     kmt->sem_init(&fill, "fill", 0);
     for (int i = 0; i < 4; i++) // 4 个生产者
-        kmt->create(task_alloc(), "producer", producer, NULL);
+        kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
     for (int i = 0; i < 5; i++) // 5 个消费者
-        kmt->create(task_alloc(), "consumer", consumer, NULL);
+        kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
 }
 #else
 static void os_init()
