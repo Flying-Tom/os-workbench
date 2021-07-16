e6d539ec2c83444a028548af57552f66f96a83bc
tracer-nju <tracer@nju.edu.cn>
2021-07-17 00:39:18 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 83f4889bd6c04e62bc3685aad67170ed   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  00:39:18 up 14:19,  0 users,  load average: 0.07, 0.03, 0.00

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -8,10 +8,10 @@ int main(int argc, char* argv[])
     FILE* fp = fopen(disk_path, "rb");
     panic(fp, "Fail to open the disk :%s", disk_path);
     fseek(fp, 0, SEEK_END);
-    uint32_t disk_size = ftell(fp);
+    uint32_t img_size = ftell(fp);
     int fd = open(disk_path, O_RDONLY);
 
-    uintptr_t img_addr = (uintptr_t)mmap(NULL, disk_size, PROT_READ, MAP_SHARED, fd, 0);
+    uintptr_t img_addr = (uintptr_t)mmap(NULL, img_size, PROT_READ, MAP_SHARED, fd, 0);
     fat_header* disk = (fat_header*)img_addr;
 
     panic(strncmp((char*)disk->BS_FilSysType, "FAT32", 5) == 0, "FAT header Error | BS_FilSysTypem : %s", (char*)disk->BS_FilSysType);
@@ -20,6 +20,10 @@ int main(int argc, char* argv[])
     void* cluster_addr = (void*)(img_addr + (disk->BPB_RsvdSecCnt + disk->BPB_NumFATs * disk->BPB_FATSz32 + disk->BPB_HiddSec) * disk->BPB_BytsPerSec);
     close(fd);
     fclose(fp);
+
+    uintptr_t addr;
+    for (addr = (uintptr_t)cluster_addr; addr < img_addr + img_size; addr += sizeof(DIR_t)) {
+    }
 }
 
 __attribute__((constructor)) void struct_check()
@@ -28,5 +32,5 @@ __attribute__((constructor)) void struct_check()
     panic(sizeof(bmp_header) == 0x36, "bad bmp_header");
     panic(sizeof(DIR_t) == 32, "bad DIR");
     panic(sizeof(LDIR_t) == 32, "bad LDIR");
-    printf("hello world\n");
+    //printf("hello world\n");
 }
