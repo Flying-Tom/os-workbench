a04db00a7fcc2a723b68116b44d3e9af371d1b80
tracer-nju <tracer@nju.edu.cn>
2021-07-17 13:33:15 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 0e2567b8e17349f98c6069d6d5f34128   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  13:33:15 up  2:49,  0 users,  load average: 0.11, 0.09, 0.04

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,5 +1,7 @@
 #include "frecov.h"
 
+char name_buf[256];
+
 unsigned char ChkSum(unsigned char* pFcbName)
 {
     short FcbNameLen;
@@ -40,6 +42,8 @@ int main(int argc, char* argv[])
 
         if ((strncmp((char*)(dir->DIR_Name + 8), "bmp", 3) == 0 || strncmp((char*)(dir->DIR_Name + 8), "BMP", 3) == 0) && dir->DIR_NTRes == 0) {
             //printf("%s\n", dir->DIR_Name);
+
+            memset(name_buf, '\0', sizeof(name_buf));
             bool isLDIR = false;
             for (int i = 0; i < 7; i++) {
                 if (!isdigit(dir->DIR_Name[i]) && !isupper(dir->DIR_Name[i]) && !islower(dir->DIR_Name[i])) {
@@ -49,7 +53,31 @@ int main(int argc, char* argv[])
             }
             if (isLDIR)
                 continue;
-            //printf("%c\n", dir->DIR_Name[0]);
+
+            unsigned char chksum = ChkSum((unsigned char*)dir->DIR_Name);
+
+            LDIR_t* ldir = (LDIR_t*)(addr - sizeof(DIR_t));
+            if (ldir->LDIR_Attr != ATTR_LONG_NAME)
+                continue;
+
+            int name_buf_cnt = 0;
+            for (int cnt = 1, i = 1; (uintptr_t)(ldir) >= (uintptr_t)cluster_addr && i <= 10; i++) {
+                if (ldir->LDIR_Chksum == chksum && ldir->LDIR_Ord & 0x0f == cnt) {
+                    cnt++;
+                    for (int j = 0; j < 5; j++)
+                        name_buf[name_buf_cnt++] = ldir->LDIR_Name1[j];
+                    for (int j = 0; j < 6; j++)
+                        name_buf[name_buf_cnt++] = ldir->LDIR_Name1[j];
+                    for (int j = 0; j < 2; j++)
+                        name_buf[name_buf_cnt++] = ldir->LDIR_Name1[j];
+                }
+                if ((ldir->LDIR_Ord & 0x40) && ldir->LDIR_Chksum == chksum)
+                    break;
+                else
+                    ldir--;
+            }
+
+            printf("%s\n", name_buf);
         }
     }
 }
