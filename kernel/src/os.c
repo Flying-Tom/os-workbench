64c181152b2326613e9d2293f637c4977b40a1c3
tracer-nju <tracer@nju.edu.cn>
2021-07-19 14:53:31 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 59b7a0df7be04b729dbb5dce19f04f26   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-61-generic       Architecture: x86-64  14:53:31 up  4:41,  0 users,  load average: 0.22, 0.22, 0.25

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -42,14 +42,14 @@ static void os_init()
     pmm->init();
     kmt->init();
 
-    puts("Start Testcase");
+    printf("Start Testcase\n");
     kmt->sem_init(&empty, "empty", 5); // 缓冲区大小为 5
     kmt->sem_init(&fill, "fill", 0);
     for (int i = 0; i < 4; i++) // 4 个生产者
         kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
     for (int i = 0; i < 5; i++) // 5 个消费者
         kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
-    puts("End Testcase");
+    printf("End Testcase\n");
 }
 #else
 static void os_init()
