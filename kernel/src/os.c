5a60435f7c280a84cae60552d391bd53c0afadf6
tracer-nju <tracer@nju.edu.cn>
2021-07-19 16:36:05 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 59b7a0df7be04b729dbb5dce19f04f26   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-61-generic       Architecture: x86-64  16:36:05 up  6:24,  0 users,  load average: 0.25, 0.24, 0.20

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -4,6 +4,20 @@
 
 static trap_handler_t trap_handlers[SEQ_MAX][TRAP_HANDLER_MAX_NUM];
 
+static void tty_reader(void* arg)
+{
+    device_t* tty = dev->lookup(arg);
+    char cmd[128], resp[128], ps[16];
+    snprintf(ps, 16, "(%s) $ ", arg);
+    while (1) {
+        tty->ops->write(tty, 0, ps, strlen(ps));
+        int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
+        cmd[nread] = '\0';
+        sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
+        tty->ops->write(tty, 0, resp, strlen(resp));
+    }
+}
+
 static void trap_init()
 {
     for (int i = SEQ_MIN; i < SEQ_MAX; i++) {
@@ -44,12 +58,17 @@ static void os_init()
     dev->init();
 
     printf("Start Testcase\n");
+    /*
     kmt->sem_init(&empty, "empty", 5); // 缓冲区大小为 5
     kmt->sem_init(&fill, "fill", 0);
     for (int i = 0; i < 4; i++) // 4 个生产者
         kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
     for (int i = 0; i < 5; i++) // 5 个消费者
         kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
+    */
+
+    kmt->create(pmm->alloc(sizeof(task_t)), "tty_reader", tty_reader, "tty1");
+    kmt->create(pmm->alloc(sizeof(task_t)), "tty_reader", tty_reader, "tty2");
     printf("End Testcase\n");
 }
 #else
