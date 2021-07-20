fb5f89b30d42fb8797bb2cca6bedbe77ebb06da1
tracer-nju <tracer@nju.edu.cn>
2021-07-21 03:08:24 +0800

    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5613f5131b6d49cd840e88122c685c9a   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-61-generic       Architecture: x86-64  03:08:24 up 14:05,  0 users,  load average: 0.50, 0.20, 0.17

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -60,6 +60,7 @@ static Context* kmt_schedule(Event e, Context* c)
     } else {
         idle_task.running = 1;
         cur_task = &idle_task;
+        printf("fuck");
     }
     kmt->spin_unlock(&task_lock);
     return cur_task->context;
